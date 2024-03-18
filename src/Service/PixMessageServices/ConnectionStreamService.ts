cda36bfc1ed2173173d1fa08fd97fc6e7b65bf51
Simeone-Holanda <simeone.10s10@gmail.com>
2024-03-18 13:51:40 -0300

    style: Removed unnecessary import.

--- src/Service/PixMessageServices/ConnectionStreamService.ts
+++ src/Service/PixMessageServices/ConnectionStreamService.ts
@@ -1,4 +1,3 @@
-import { v4 as uuidv4 } from 'uuid'
 import HttpError from '../../Errors/httpError'
 import { IPixMessageRepository } from '../../repositories/interfaces/IPixMessage'
 import IInteractionRepository from '../../repositories/interfaces/IInteractionRepository'
