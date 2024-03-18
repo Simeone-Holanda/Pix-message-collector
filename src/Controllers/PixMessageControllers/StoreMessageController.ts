2025ba93dcfe4ab3f3c5866ddd1cbeb7a565b9d7
Simeone-Holanda <simeone.10s10@gmail.com>
2024-03-18 13:33:56 -0300

    refactor: Added some http code treatments.

--- src/Controllers/PixMessageControllers/StoreMessageController.ts
+++ src/Controllers/PixMessageControllers/StoreMessageController.ts
@@ -3,6 +3,7 @@ import {
   StoreMessageService,
   storeMessageServiceInstance,
 } from '../../Service/PixMessageServices/StorageMessageService'
+import HttpError from '../../Errors/httpError'
 
 class StoreMessageController {
   private readonly storeMessageService: StoreMessageService
@@ -11,12 +12,26 @@ class StoreMessageController {
   }
 
   async execute(request: Request, response: Response) {
-    console.log('adicionando mensagens')
-    const { ispb, number } = request.params
-    await this.storeMessageService.execute(ispb, number)
-    return response
-      .status(201)
-      .json({ message: 'Mensagens PIX adicionadas com sucesso.' })
+    try {
+      const { ispb, number } = request.params
+      if (!ispb || !number)
+        throw new HttpError('Ispb or number not found. ', 400)
+      await this.storeMessageService.execute(ispb, number)
+      return response
+        .status(201)
+        .json({ message: 'Mensagens PIX adicionadas com sucesso.' })
+    } catch (error) {
+      if (error instanceof HttpError) {
+        return response.status(error.statusCode).send({
+          message: error.message,
+        })
+      } else {
+        return response.status(500).json({
+          message:
+            'An internal server error has occurred, contact the developer. ',
+        })
+      }
+    }
   }
 }
 
